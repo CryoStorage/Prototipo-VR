e4157733e792c7e31592791299d734066d12ad2e
Cs00 <iracoon2014@gmail.com>
2022-08-25 16:26:33 -0500

    Deleted duplicate project

--- ProyectoVR/Assets/Scripts/Player_Crawl.cs
+++ ProyectoVR/Assets/Scripts/Player_Crawl.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Player_Crawl : MonoBehaviour
 {
+    bool l = false;
+    bool r = true;
+    Vector3 dir;
+    [SerializeField]float speed;
+    CharacterController con;
+    Vector3 friction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        con = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Crawl();
+        Move();
+
+        Debug.Log(l);
+        Debug.Log(r);
+    }
+
+    void Crawl()
+    {
+        if(Input.GetMouseButtonDown(0) && l == true)
+        {
+            dir +=(Vector3.forward * speed * Time.fixedDeltaTime);
+            l = false;
+            r = true;
+        }
+        if(Input.GetMouseButtonDown(1) && r == true)
+        {
+            dir += (Vector3.forward * speed * Time.fixedDeltaTime);
+            r = false;
+            l = true;
+        }
+    }
+
+    void Move()
+    {
+        friction = ((dir * -1) / 10);  
+        con.Move(dir);
+
     }
 }
