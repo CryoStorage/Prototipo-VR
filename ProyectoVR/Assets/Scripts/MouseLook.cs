c041536cf8fbaa2716f22d5f5f7855cc4deafb96
Raymundo Mosqueda <iracoon2014@gmail.com>
2022-08-25 08:56:46 -0500

    UnityProject Added

---
+++ ProyectoVR/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    float sens = 100f;
+    public transform pBody;
+    float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockmode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = MathF.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot,0,0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
