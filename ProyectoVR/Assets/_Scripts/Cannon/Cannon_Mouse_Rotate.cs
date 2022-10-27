0bf9012d1a86f1996c2cfb6bbcafc908d43141a2
Cs00 <iracoon2014@gmail.com>
2022-10-27 14:50:34 -0500

    Removed Mock HMD Canon Vr/mouse split

--- ProyectoVR/Assets/_Scripts/Cannon/Cannon_Mouse_Rotate.cs
+++ ProyectoVR/Assets/_Scripts/Cannon/Cannon_Mouse_Rotate.cs
@@ -1,33 +1,30 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cannon_Rotate : Gun_Rotate
+public class Cannon_Mouse_Rotate : Gun_Rotate
 {
     [SerializeField]private float maxAngleX, maxAngleY = 45f;
     private float _minAngleX, _minAngleY;
     [Tooltip("Sensitivity Multiplier")]
     [SerializeField]private float sens = 1;
-
-    [SerializeField] private GameObject handle;
-    private Vector3 _handlePos;
-
+    
+    
     protected override void Start()
     {
         base.Start();
-        Prepare();
         _minAngleX = maxAngleX - 360;
         _minAngleY = maxAngleY - 360;
 
     }
 
+    // Update is called once per frame
     protected override void Update()
     {
-        LookAtRotate();
-
+        MouseRotate();
     }
-
+    
+    
     private void MouseRotate()
     {
         mouseX += Input.GetAxis("Mouse X") * sens;
@@ -41,19 +38,4 @@ public class Cannon_Rotate : Gun_Rotate
         //     Mathf.Clamp(eulers.y, _minAngleY, maxAngleY),0f);
         transform.localEulerAngles = eulers;
     }
-
-    private void LookAtRotate()
-    { 
-        transform.LookAt(-_handlePos);
-        _handlePos = handle.transform.position;
-
-    }
-
-    private void Prepare()
-    {
-
-    }
-    
-    
-
 }
